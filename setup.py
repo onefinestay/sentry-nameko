4affbdeb867d23112f88541421b2b2ac2e57a70d
Shaun Stanworth <shaun.stanworth@onefinestay.com>
2014-01-16 12:51:26 +0000

    Include templates via MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='sentry-nameko',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['test', 'test.*']),
     url='http://github.com/onefinestay/sentry-nameko',
     author='onefinestay',
